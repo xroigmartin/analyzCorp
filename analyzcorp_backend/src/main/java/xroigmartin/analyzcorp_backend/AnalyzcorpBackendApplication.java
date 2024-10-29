228737598fa37a96abc4e0df049e3e86bad75d04
Xavier Roig <xroigmartin@gmail.com>
2024-10-29 07:32:17 +0100

    Create java application

---
+++ analyzcorp_backend/src/main/java/xroigmartin/analyzcorp_backend/AnalyzcorpBackendApplication.java
@@ -0,0 +1,13 @@
+package xroigmartin.analyzcorp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzcorpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzcorpBackendApplication.class, args);
+	}
+
+}
