16818fbdacc68b9be45635a1765ed08e7f5af91d
Xavier Roig <38437873+xroigmartin@users.noreply.github.com>
2024-10-30 20:24:15 +0100

    Create java application (#1) (#2)
    * Create java application
    
    * create basic configuration of project

---
+++ analyzcorp_backend/src/test/java/xroigmartin/analyzcorp_backend/AnalyzcorpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xroigmartin.analyzcorp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzcorpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
