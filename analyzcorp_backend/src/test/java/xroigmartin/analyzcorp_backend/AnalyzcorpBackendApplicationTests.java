c78b49ecda2c01d07eef861347b6ad0d9ec76347
Xavier Roig <xroigmartin@gmail.com>
2024-12-08 23:02:46 +0100

    fix test Spring context

--- analyzcorp_backend/src/test/java/xroigmartin/analyzcorp_backend/AnalyzcorpBackendApplicationTests.java
+++ analyzcorp_backend/src/test/java/xroigmartin/analyzcorp_backend/AnalyzcorpBackendApplicationTests.java
@@ -1,9 +1,12 @@
 package xroigmartin.analyzcorp_backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {AnalyzcorpBackendApplication.class})
+@ImportAutoConfiguration(exclude = {FlywayAutoConfiguration.class})
 class AnalyzcorpBackendApplicationTests {
 
 	@Test
