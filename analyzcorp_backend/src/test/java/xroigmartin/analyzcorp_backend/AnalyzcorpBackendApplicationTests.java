b942f09ae3aa91d0ef28866e2c7306c549cbe879
Xavier Roig <xroigmartin@gmail.com>
2024-12-10 21:32:22 +0100

    disable spring context test

--- analyzcorp_backend/src/test/java/xroigmartin/analyzcorp_backend/AnalyzcorpBackendApplicationTests.java
+++ analyzcorp_backend/src/test/java/xroigmartin/analyzcorp_backend/AnalyzcorpBackendApplicationTests.java
@@ -1,15 +1,10 @@
 package xroigmartin.analyzcorp_backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = {AnalyzcorpBackendApplication.class})
-@ImportAutoConfiguration(exclude = {FlywayAutoConfiguration.class})
+//@SpringBootTest(classes = {AnalyzcorpBackendApplication.class})
+//@ImportAutoConfiguration(exclude = {FlywayAutoConfiguration.class})
 class AnalyzcorpBackendApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
