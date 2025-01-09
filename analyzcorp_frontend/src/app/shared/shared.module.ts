6bacfc67e0bc62217ea906d9185b6d78426a5d11
Xavier Roig <xroigmartin@gmail.com>
2025-01-09 07:04:05 +0100

    create frontend project

---
+++ analyzcorp_frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PrimengModule } from '../primeng/primeng.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PrimengModule
+  ]
+})
+export class SharedModule { }
