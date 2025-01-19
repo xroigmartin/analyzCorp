0ce1d875b0230ed63b7ebc03c8f5c45f6a52cbc4
Xavier Roig <xroigmartin@gmail.com>
2025-01-19 09:40:26 +0100

    update primeng and show taost when create a bank account

--- analyzcorp_frontend/src/app/personal_economy/bank/pages/new-account/new-account.component.ts
+++ analyzcorp_frontend/src/app/personal_economy/bank/pages/new-account/new-account.component.ts
@@ -9,22 +9,45 @@ import {Card} from 'primeng/card';
 import {FloatLabel} from 'primeng/floatlabel';
 import {InputText} from 'primeng/inputtext';
 import {Button} from 'primeng/button';
+import {Toast} from 'primeng/toast';
+import {Avatar} from 'primeng/avatar';
+import {Message} from 'primeng/message';
+import {MessageService} from 'primeng/api';
 
 @Component({
   standalone: true,
   selector: 'personal-economy-new-account',
-  imports: [ReactiveFormsModule, CommonModule, FormsModule, Card, FloatLabel, InputText, Button],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule, Card, FloatLabel, InputText, Button, Toast, Avatar],
   templateUrl: './new-account.component.html',
-  styleUrl: './new-account.component.css'
+  styleUrl: './new-account.component.css',
+  providers: [MessageService]
 })
 export class NewAccountComponent {
+
+  constructor(private messageService: MessageService) {
+  }
 
   public accountModel: CreateAccountDTO = {
     bankName: '',
     iban: '',
     alias: ''
   }
+
+  public visible: boolean = false;
+
   onSubmit(): void  {
+    this.messageService.add({ key: 'confirm', sticky: true, severity: 'success', summary: 'Bank account create successfully' })
+    this.visible = true;
     console.log(this.accountModel);
+  }
+
+  onConfirm(): void{
+    this.messageService.clear('confirm');
+    this.visible = false;
+  }
+
+  onReject(): void {
+    this.messageService.clear('confirm');
+    this.visible = false;
   }
 }
