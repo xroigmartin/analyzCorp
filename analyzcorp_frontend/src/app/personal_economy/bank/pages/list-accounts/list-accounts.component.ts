18af55e327996931e51f5d637556ab2108e93f16
Xavier Roig <xroigmartin@gmail.com>
2025-01-21 22:32:12 +0100

    list bank accounts

--- analyzcorp_frontend/src/app/personal_economy/bank/pages/list-accounts/list-accounts.component.ts
+++ analyzcorp_frontend/src/app/personal_economy/bank/pages/list-accounts/list-accounts.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {TableModule} from 'primeng/table';
 import {AccountDTO} from '../../interfaces/AccountDTO.interfaces';
 import {Button} from 'primeng/button';
 import {RouterLink} from '@angular/router';
+import {BankAccountService} from '../../services/bank-account.service';
+import {ApiResponse} from '../../../../shared/interfaces/ApiResponse.interface';
 
 @Component({
   standalone: true,
@@ -11,7 +13,17 @@ import {RouterLink} from '@angular/router';
   templateUrl: './list-accounts.component.html',
   styleUrl: './list-accounts.component.css'
 })
-export class ListAccountsComponent {
+export class ListAccountsComponent implements OnInit{
+
+  private bankAccountService: BankAccountService = inject(BankAccountService);
 
   accounts: AccountDTO[] = [];
+
+  ngOnInit(): void {
+    this.bankAccountService.findAllBankAccounts().subscribe({
+      next: (apiResponse: ApiResponse<AccountDTO[]>) => {
+        this.accounts = apiResponse.data;
+      }
+    });
+  }
 }
